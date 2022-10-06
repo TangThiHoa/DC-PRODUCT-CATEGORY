05dc32a16ce8a190f35b63f889a9bace15fe67ea
TangThiHoa <hoarua592001@gmail.com>
2022-10-06 11:56:39 +0700

    truy vấn

--- src/main/java/com/example/testcategory/main/Test.java
+++ src/main/java/com/example/testcategory/main/Test.java
@@ -7,15 +7,6 @@ public class Test {
     public static void main(String[] args) {
 
 
-        CategoryDAO.getInstance().searchByCategory("A").forEach(System.out::println);
-
-//        Test searchByCategoryDetail
-//        CategoryDetailDao.getInstance().searchByCategoryDeTailName("s").forEach(System.out::println);
-
-//        Test searchByProduct
-//        ProductDAO.getInstance().search("Iphone").forEach(System.out::println);
-
-//        Test searchByPrice
 
     }
 }
